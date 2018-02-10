ef6346ab38353d7e0defeb576910514f8a4fa75b
Eugene Anufriev <eugene.anufriev@gmail.com>
2018-02-10 16:57:31 +0200

    cleaning and fixes

--- web/index.php
+++ web/index.php
@@ -6,7 +6,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../config/bootstrap.php');
-require(__DIR__ . '/../globals.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main.php'),
