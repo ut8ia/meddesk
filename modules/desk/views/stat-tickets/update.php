9a588928a0685ce7a5142426fff7d4928eb57166
Eugene Anufriev <eugene.anufriev@gmail.com>
2018-10-24 12:16:44 +0300

    fix translation

--- modules/desk/views/stat-tickets/update.php
+++ modules/desk/views/stat-tickets/update.php
@@ -4,12 +4,12 @@
 /* @var $model app\modules\desk\models\Meets */
 
 $this->title = Yii::t('desk', 'Update {modelClass}: ', [
-        'modelClass' => 'Meets',
+        'modelClass' => 'statistic ticket',
     ]) . $model->id;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('desk', 'Meets'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('desk', 'statistic tickets'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('desk', 'Update');
-$this->blocks['content-header'] = Yii::t('desk', 'Meets');
+$this->blocks['content-header'] = Yii::t('desk', 'statistic ticket');
 ?>
 
 
